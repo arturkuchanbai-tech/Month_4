021a3b8e47dde514fd1cafc290bc09970412275a
Artur <arturkuchanbai@gmail.com>
2026-02-08 16:57:12 +0600

    ok

--- Book/models.py
+++ Book/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 class Book(models.Model):
-    title = models.CharField(max_length=200)                     # Название
-    author = models.CharField(max_length=100)                    # Автор
-    published_date = models.DateField(null=True, blank=True)     # Дата публикации
-    isbn = models.CharField(max_length=13, unique=True)          # ISBN
-    pages = models.IntegerField(null=True, blank=True)           # Количество страниц
-    cover = models.ImageField(upload_to='covers/', null=True, blank=True)       # Обложка
-    file = models.FileField(upload_to='books_files/', null=True, blank=True)    # Файл книги
-    description = models.TextField(null=True, blank=True)        # Описание
-    publisher_email = models.EmailField(null=True, blank=True)    # Email издателя
-    info_url = models.URLField(null=True, blank=True)            # Ссылка на сайт книги
+    title = models.CharField(max_length=200)               
+    author = models.CharField(max_length=100)                  
+    published_date = models.DateField(null=True, blank=True)     
+    isbn = models.CharField(max_length=13, unique=True)          
+    pages = models.IntegerField(null=True, blank=True)           
+    cover = models.ImageField(upload_to='covers/', null=True, blank=True)       
+    file = models.FileField(upload_to='books_files/', null=True, blank=True)   
+    description = models.TextField(null=True, blank=True)      
+    publisher_email = models.EmailField(null=True, blank=True)  
+    info_url = models.URLField(null=True, blank=True)           
--- relation_db/admin.py
+++ relation_db/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from . import  models
-
 admin.site.register(models.Tag)
 admin.site.register(models.TouristCategory)
 admin.site.register(models.Person)
